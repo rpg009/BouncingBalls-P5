b0feafdae86565fefcf0f2c7781a078108338652
rpg009 <115803607+rpg009@users.noreply.github.com>
2022-12-13 21:20:22 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,159 @@
+/* This code was copied from the handout given by Professor Meng. 
+
+The Changes I made include:
+1. Change in the text box size and text
+2. Change in the randomness of the ball speed
+3. Adding start your engines audio, implying a race betweeen the balls.
+*/
+
+// making the audio file variable
+let Engines
+
+// preload
+function preload(){
+  Engines = loadSound("StartYourEngines.mp3")
+}
+
+const BOX_WIDTH  = 600;  // textbox dimensions
+const BOX_HEIGHT = 75;
+
+var balls = [];
+var sound;
+var testBall;
+
+function setup() {
+
+//  createCanvas(windowWidth, windowHeight);
+  createCanvas(600,400)
+
+  
+  noStroke();
+  
+  Engines.play()
+  
+  
+  for (var ballNum = 0; ballNum < 10; ballNum++) {
+  	balls[ballNum] = new Ball();  
+  }
+
+  let y = height;
+  testBall = new Ball();
+  testBall.size = 50;
+  testBall.ballX = 220;  // if ballX == 225, the ball just slides over the right edge
+  testBall.ballY = 300;
+  testBall.red = 0;
+  testBall.blue = 0;
+  testBall.green = 0;
+  testBall.speedX = 0;
+  testBall.speedY = 1.2;
+}
+
+function createBox() {
+  // prepare a box first
+  strokeWeight(4);
+  rect(0, 0, BOX_WIDTH, BOX_HEIGHT);
+  
+  textSize(32);           // size of the text (pixels)
+  fill(0, 102, 153);      // fill() takes R,G,B values as the color
+  // draw the text in the box (x,y,width,height) with the color in fill()
+  textAlign(CENTER);
+  text('Start Your Engines!', BOX_WIDTH/2, BOX_HEIGHT/2);   
+ 
+}
+
+function draw() {
+  
+  
+
+  background(255);
+  createBox();
+  
+  testBallMove();  // a special ball to test corner collision
+  
+  for (var ballNum = 0; ballNum < balls.length; ballNum++) {
+    balls[ballNum].display();
+    balls[ballNum].checkForHitWall();
+    balls[ballNum].checkForHitBox();
+    balls[ballNum].moveBall();
+    
+    if (mouseIsPressed) {
+      balls[ballNum].randomize()
+    }
+  }
+}
+
+function testBallMove() {
+  
+  testBall.display();
+  testBall.checkForHitWall();
+  testBall.checkForHitBox();
+  testBall.moveBall();
+}
+
+class Ball { // Constructor
+  
+  constructor() {
+    // initial position
+    this.ballX = random(50, width)
+    this.ballY = random(100, height)
+    
+    // Dictates velocity + direction
+    this.speedY = random(-5, 15);
+    this.speedX = random(-5, 15);
+    
+    this.size = random(100);
+    
+    // How transparent the ball is
+    this.alpha = 100
+    
+    // RGB values for color
+    this.red   = random(255);
+    this.green = random(255);
+    this.blue  = random(255)
+  }
+  
+  display() {
+    fill(this.red, this.green, this.blue, this.alpha);
+    ellipse(this.ballX, this.ballY, this.size);
+  }
+  
+  randomize() {
+    this.speedY = random(-5, 5);
+    this.speedX = random(-5, 5);
+  }
+  
+  checkForHitWall() {
+  
+    let radius = this.size / 2;
+    if ((this.ballY+radius) > height || (this.ballY-radius) < 0) {
+  	  this.speedY = -this.speedY;  
+  	}
+    if ((this.ballX+radius) > width  || (this.ballX-radius) < 0) {
+      this.speedX = -this.speedX;  
+    }
+  }
+  
+  checkForHitBox() {
+    
+    let radius = this.size / 2;
+
+//    if (((this.ballX-radius) < BOX_WIDTH && (this.ballY-radius) < BOX_HEIGHT) || d < radius) {
+    if (((this.ballX-radius) < BOX_WIDTH && (this.ballY-radius) < BOX_HEIGHT)) {
+      // bump into the textbox, need to reverse direction
+      this.reverseBall();
+    }
+  }
+  
+  reverseBall() {
+    
+      this.speedX = -this.speedX;
+      this.speedY = -this.speedY;    
+  }
+  
+  moveBall() {
+
+    this.ballX += this.speedX;
+  	this.ballY += this.speedY;
+  }
+  
+}
